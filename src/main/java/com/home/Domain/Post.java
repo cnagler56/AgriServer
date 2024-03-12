8a78e7c1a3cc049f47a6ee5f97a54947570f56fb
cnagler56 <chrisnagelmg@gmail.com>
2024-03-11 20:26:27 -0500

    add domains

---
+++ src/main/java/com/home/Domain/Post.java
@@ -0,0 +1,70 @@
+package com.home.Domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@JsonProperty
+	Long idposts;
+	
+	@JsonProperty
+	String title;
+	
+	@JsonProperty
+	String body;
+	
+	@JsonProperty
+	String time;
+
+	@JsonProperty
+	Long userId;
+
+	public Long getIdposts() {
+		return idposts;
+	}
+
+	public void setIdposts(Long idposts) {
+		this.idposts = idposts;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	
+}
---
+++ src/main/java/com/home/Domain/corndata2023.java
@@ -0,0 +1,5 @@
+package com.home.Domain;
+
+public class corndata2023 {
+
+}
